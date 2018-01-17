62f09420e75254306ff146fe76c04eec23b1c10a
Vinzenz Aubry <vinzenzaubry@gmail.com>
2018-01-17 11:17:19 +0100

    Added settings && first css layout

--- dev/blockViewport.js
+++ dev/blockViewport.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
 
-function BlockViewport() {
-	this.params = {}
-
-	this.init();
+function BlockViewport(settings) {
+	this.settings = settings;
+	this.inject();
+	this.addEventListeners();
+	this.resize();
 }
 
-BlockViewport.prototype.init = function () {
+BlockViewport.prototype.inject = function () {
+	for (let i = 0; i < this.settings.length; i++) {
+		const blocker = this.settings[i];
+
+		//set Settings
+		let settings = {};
+		settings.color = blocker.color || '#000000';
+		settings.fontColor = blocker.fontColor || '#FFFFFF';
+		settings.textDesktop = blocker.textDesktop || 'This viewport is blocked. Please change your browser size';
+		settings.textMobile = blocker.textMobile || settings.textDesktop;
+		settings.widthStart = blocker.widthStart || 0;
+		settings.widthEnd = blocker.widthEnd || 9999;
+
+		// prepare div to be injected
+		console.log(settings);
+	}
+}
+
+BlockViewport.prototype.addEventListeners = function () {
+
+}
+
+BlockViewport.prototype.resize = function () {
 
 }
